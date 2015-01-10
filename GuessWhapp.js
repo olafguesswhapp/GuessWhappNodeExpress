1f8c2afaeb608eaa6e680d90bd207482cea97f61
Olaf Peters <olaf@guesswhapp.de>
2015-01-10 11:43:26 +0100

    Inital Commit

---
+++ GuessWhapp.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+
+var gwcode = [647, 646, 730];
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+	.create({
+			defaultLayout:'main'});
+	
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	var randomGWcode = gwcode[Math.floor(Math.random() * gwcode.length)];
+	res.render('home', {gwcode: randomGWcode});
+});
+
+app.get('/team', function(req,res){
+	res.render('team', {layout: 'sub'});
+});
+
+app.get('/wieesfunktioniert', function(req,res){
+	res.render('wieesfunktioniert', {layout: 'sub'});
+});
+
+app.get('/impressum', function(req,res){
+	res.render('impressum', {layout: 'sub'});
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
---
+++ README.md
@@ -0,0 +1,5 @@
+# GuessWhapp DE Site
+
+## built with node.js, Express and Handlebars.js
+
+This is the first public site created with the above stack
